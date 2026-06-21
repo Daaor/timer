0ea06ef855f4b977d646098efdc90812561ab006
Daaor <praisedaaor@gmail.com>
2026-06-21 20:44:15 +0100

    Finalized the display, and defined Javascript Variables

---
+++ public/index.js
@@ -0,0 +1,12 @@
+let isRunning = false;
+let myTime = 0;
+let intervalId;
+
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const start = document.getElementById("start");
+const pause = document.getElementById("pause");
+const reset = document.getElementById("reset");
+
